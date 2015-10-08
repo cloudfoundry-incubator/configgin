f9c8aedeb02e0aaf0d98e84529a17fb7dd20437e
Vlad Iovanov <viovanov@hotmail.com>
2015-10-08 01:03:14 +0000

    Making bash script look better. thnx Ingy

--- package.sh
+++ package.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ -z "$CONFIGGIN_VERSION" ]; then
+set -e
+
+if [[ -z $CONFIGGIN_VERSION ]]; then
 	CONFIGGIN_VERSION=0.0.0
 fi
 
@@ -12,36 +14,39 @@ rsync -a ./ ./output/${app_name}/lib/app --exclude ./output
 
 mkdir -p ./output/packaging
 
-pushd ./output
-	pushd ./packaging
+(
+	cd ./output
+	(
+		cd ./packaging
 		curl -L -O --fail http://d6r77u77i8pq3.cloudfront.net/releases/${traveling_ruby}
-	popd
+	)
 	mkdir -p ${app_name}/lib/ruby
 	tar -xzf packaging/${traveling_ruby} -C ${app_name}/lib/ruby
-popd
+)
 
-cat >./output/${app_name}/configgin <<EOL
+cat >./output/${app_name}/configgin <<'EOL'
 #!/bin/bash
 set -e
 
 # Figure out where this script is located.
-SELFDIR="\`dirname \"\$0\"\`"
-SELFDIR="\`cd \"\$SELFDIR\" && pwd\`"
+SELFDIR="`dirname \"$0\"`"
+SELFDIR="`cd "$SELFDIR" && pwd`"
 
 # Tell Bundler where the Gemfile and gems are.
-export BUNDLE_GEMFILE="\$SELFDIR/lib/vendor/Gemfile"
+export BUNDLE_GEMFILE="$SELFDIR/lib/vendor/Gemfile"
 unset BUNDLE_IGNORE_CONFIG
 
 # Run the actual app using the bundled Ruby interpreter.
-exec "\$SELFDIR/lib/ruby/bin/ruby" -rbundler/setup "\$SELFDIR/lib/app/bin/config-gen"
+exec "$SELFDIR/lib/ruby/bin/ruby" -rbundler/setup "$SELFDIR/lib/app/bin/config-gen"
 EOL
 
 chmod +x ./output/${app_name}/configgin
 
-pushd ./output/${app_name}/lib/app
+(
+	cd ./output/${app_name}/lib/app
 	BUNDLE_IGNORE_CONFIG=1 ../ruby/bin/bundle install --path ../vendor
 	rm -rf ../vendor/*/*/cache/*
-popd
+)
 
 cp ./Gemfile ./output/${app_name}/lib/vendor
 cp ./Gemfile.lock ./output/${app_name}/lib/vendor
@@ -56,3 +61,4 @@ EOL
 
 tar -czf ./output/${app_name}.tar.gz ./output/${app_name}
 
+# vim: set ts=2:
