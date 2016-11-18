f520d68b343182d72be050cf96f1cc74a5af528c
Alex Harford <alex.harford@hpe.com>
2016-11-18 09:24:54 -0800

    HCF-1053 Cache results of mustache parsing

--- lib/environment_config_transmogrifier.rb
+++ lib/environment_config_transmogrifier.rb
@@ -5,6 +5,8 @@ require 'shellwords'
 # EnvironmentConfigTransmogrifier uses environment variables to generate config values
 # for specific keys.
 module EnvironmentConfigTransmogrifier
+  @@memoize_mustache = {}
+
   # NoEscapeMustache does not escape these characters: & \ " < > '
   class NoEscapeMustache < Mustache
     # Disabling this cop because this is the function we need to override
@@ -50,12 +52,21 @@ module EnvironmentConfigTransmogrifier
   end
 
   def self.processMustacheTemplate(value, input_hash, key)
+    unless @@memoize_mustache[value].nil?
+      unless @@memoize_mustache[value][input_hash].nil?
+        return @@memoize_mustache[value][input_hash]
+      end
+    end
+
     delimiter = '{{=(( ))=}}'
     begin
       mustache_value = NoEscapeMustache.render("#{delimiter}#{value}", input_hash)
       # replace new lines with double new lines for proper new-line YAML parsing
       mustache_value = mustache_value.to_s.gsub("\n", "\n\n")
-      YAML.load(mustache_value)
+      if @@memoize_mustache[value].nil?
+        @@memoize_mustache[value] = {}
+      end
+      @@memoize_mustache[value][input_hash] = YAML.load(mustache_value)
     rescue => e
       msg = mustacheMessageFromError(e)
       raise LoadYamlFromMustacheError, "Could not load config key '#{key}': #{msg}"
